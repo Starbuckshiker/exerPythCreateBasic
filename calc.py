feac50157d26174804e78b95a26a0b09475515d6
starbuckshiker <u1304954@umail.utah.edu>
2020-09-10 12:40:26 -0600

    calc.py

---
+++ calc.py
@@ -0,0 +1,9 @@
+#simple calculator
+x = first number
+first number = input('enter first number')  
+y = second number
+second number = input('enter second number')
+
+results = (x + y)
+
+print(results)
